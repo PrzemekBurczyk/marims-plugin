b2f84db90ab297d153a8c24a0ea7a4969aa1a897
Przemek Burczyk <przemek.burczyk@gmail.com>
2015-12-13 20:16:52 +0100

    Embedded JavaFX WebView in plugin toolbar window. Loading marims.pl as the main page.

--- src/pl/edu/agh/marims/plugin/MarimsToolWindowFactory.java
+++ src/pl/edu/agh/marims/plugin/MarimsToolWindowFactory.java
@@ -5,55 +5,39 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.util.Calendar;
 
 public class MarimsToolWindowFactory implements ToolWindowFactory {
-    private JButton refreshToolWindowButton;
-    private JButton hideToolWindowButton;
-    private JLabel currentDate;
-    private JLabel currentTime;
-    private JLabel timeZone;
-    private JPanel myToolWindowContent;
     private ToolWindow toolWindow;
-
+    private JPanel toolWindowContent;
+    private JFXPanel jfxPanel;
+    private WebView webView;
+    private WebEngine webEngine;
 
     public MarimsToolWindowFactory() {
-        hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
-        refreshToolWindowButton.addActionListener(e -> MarimsToolWindowFactory.this.currentDateTime());
     }
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         this.toolWindow = toolWindow;
-        this.currentDateTime();
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(myToolWindowContent, "", false);
+        Content content = contentFactory.createContent(toolWindowContent, "", false);
         toolWindow.getContentManager().addContent(content);
+
+        Platform.runLater(this::initWebView);
     }
 
-    public void currentDateTime() {
-        // Get current date and time
-        Calendar instance = Calendar.getInstance();
-        currentDate.setText(String.valueOf(instance.get(Calendar.DAY_OF_MONTH)) + "/"
-                + String.valueOf(instance.get(Calendar.MONTH) + 1) + "/" + String.valueOf(instance.get(Calendar.YEAR)));
-        currentDate.setIcon(new ImageIcon(getClass().getResource("/images/Calendar-icon.png")));
-        int min = instance.get(Calendar.MINUTE);
-        String strMin;
-        if (min < 10) {
-            strMin = "0" + String.valueOf(min);
-        } else {
-            strMin = String.valueOf(min);
-        }
-        currentTime.setText(instance.get(Calendar.HOUR_OF_DAY) + ":" + strMin);
-        currentTime.setIcon(new ImageIcon(getClass().getResource("/images/Time-icon.png")));
-        // Get time zone
-        long gmt_Offset = instance.get(Calendar.ZONE_OFFSET); // offset from GMT in milliseconds
-        String str_gmt_Offset = String.valueOf(gmt_Offset / 3600000);
-        str_gmt_Offset = (gmt_Offset > 0) ? "GMT + " + str_gmt_Offset : "GMT - " + str_gmt_Offset;
-        timeZone.setText(str_gmt_Offset);
-        timeZone.setIcon(new ImageIcon(getClass().getResource("/images/Time-zone-icon.png")));
+    private void initWebView() {
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.load("http://marims.pl");
+        jfxPanel.setScene(new Scene(webView, 1000, 900));
     }
 }
