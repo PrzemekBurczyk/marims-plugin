054580491b2a2c1e056bd14630cf39d47c3ee0ab
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-01-03 17:27:34 +0100

    Implemented simple dashboard panel.

---
+++ src/pl/edu/agh/marims/plugin/Dashboard.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.marims.plugin;
+
+import javax.swing.*;
+
+public class Dashboard {
+    private JTextArea title;
+    private JFileChooser fileChooser;
+    private JList buildsList;
+}
