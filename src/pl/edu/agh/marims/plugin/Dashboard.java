c642ad2e1bf1d7730bf089ad654bff6e35bed1f8
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-01-03 18:08:35 +0100

    Implemented file chooser dialog.

--- src/pl/edu/agh/marims/plugin/Dashboard.java
+++ src/pl/edu/agh/marims/plugin/Dashboard.java
@@ -1,9 +1,45 @@
 package pl.edu.agh.marims.plugin;
 
-import javax.swing.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
 
-public class Dashboard {
-    private JTextArea title;
-    private JFileChooser fileChooser;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Dashboard implements ToolWindowFactory {
     private JList buildsList;
+    private JPanel contentPanel;
+    private JLabel title;
+    private JButton chooseFileButton;
+    private JLabel chooseFileLabel;
+
+    private File selectedFile;
+
+    public Dashboard() {
+        chooseFileButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int selectedOption = fileChooser.showOpenDialog(contentPanel);
+            if (selectedOption == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+                try {
+                    chooseFileLabel.setText(selectedFile.getCanonicalPath());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(contentPanel, "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+
 }
